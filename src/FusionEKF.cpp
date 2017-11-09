66c134d732b428c74baeac4788059fcfa34a2d07
leros <zhleiyu@gmail.com>
2017-11-08 21:53:04 -0500

    Change initial value for P_ to improve RMSE accuracy on vx and vy

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -67,8 +67,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.P_ = MatrixXd(4, 4);
     ekf_.P_ << 1, 0, 0, 0,
   			  0, 1, 0, 0,
-  			  0, 0, 1000, 0,
-  			  0, 0, 0, 1000;
+  			  0, 0, 10, 0,
+  			  0, 0, 0, 10;
 
     ekf_.F_ = MatrixXd(4, 4);
     ekf_.F_ << 1, 0, 1, 0,
